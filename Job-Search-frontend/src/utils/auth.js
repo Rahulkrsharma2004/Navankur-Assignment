08225fa065510e68e21331f854ac34592fd5d7d4
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-02-07 14:21:16 +0530

    done

--- Job-Search-frontend/src/utils/auth.js
+++ Job-Search-frontend/src/utils/auth.js
@@ -1,15 +1,12 @@
-// Check if user is authenticated
 export const isAuthenticated = () => {
     const user = JSON.parse(localStorage.getItem('user'));
     return user !== null;
   };
   
-  // Get current user
   export const getCurrentUser = () => {
     return JSON.parse(localStorage.getItem('user'));
   };
   
-  // Logout user
   export const logoutUser = () => {
     localStorage.removeItem('user');
   };
